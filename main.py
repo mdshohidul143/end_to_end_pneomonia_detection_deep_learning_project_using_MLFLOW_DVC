65a67adcfd9edecbcc6a988443570096b650c56b
mdshohidul143 <mdshohidulislam01311@gmail.com>
2024-02-15 14:43:33 +0600

    prepare base model added

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context ### this is for unknown errors handling
+
 from cnnClassifier import logger
 from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+
 
 STAGE_NAME = "Data Ingestion stage"
 
@@ -9,6 +14,19 @@ try:
     obj = DataIngestionTrainingPipeline()
     obj.main()
     logger.info(f">>>> stage {STAGE_NAME} completed <<<<<<<<\n\nx========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+
+STAGE_NAME = "Prepare base model"
+try:
+    logger.info(f"********************")
+    logger.info(f">>>>> stage {STAGE_NAME} started <<<<<<")
+    prepare_base_model = PrepareBaseModelTrainingPipeline()
+    prepare_base_model.main()
+    logger.info(f">>>>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx========x")
 except Exception as e:
     logger.exception(e)
     raise e
