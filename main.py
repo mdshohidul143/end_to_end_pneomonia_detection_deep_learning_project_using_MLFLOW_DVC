0053196bb5181b3bc34c72ac21fad01aea1173e1
mdshohidul143 <mdshohidulislam01311@gmail.com>
2024-02-16 16:22:46 +0600

    Model trainer added

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-import ssl
-ssl._create_default_https_context = ssl._create_unverified_context ### this is for unknown errors handling
+# import ssl
+# ssl._create_default_https_context = ssl._create_unverified_context ### this is for unknown errors handling
 
 from cnnClassifier import logger
 from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
-
+from cnnClassifier.pipeline.stage_03_model_trainer import ModelTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 
@@ -29,4 +29,15 @@ try:
     logger.info(f">>>>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx========x")
 except Exception as e:
     logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Training"
+try:
+    logger.info(f"*************")
+    logger.info(f">>>>> stage {STAGE_NAME} started <<<<<<<<<<<")
+    model_trainer = ModelTrainingPipeline()
+    model_trainer.main()
+    logger.info(f">>>>>>>>> stage {STAGE_NAME} completed <<<<<<<<\n\nx========x")
+except Exception as e:
     raise e
